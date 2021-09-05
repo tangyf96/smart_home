469df1385dc1690ca44a61fdfc19a865411dcc6d
tangyifan <tangyf96@outlook.com>
2021-09-05 13:12:16 +0800

    add humidity sensor

---
+++ sensor/human_infrared_sensor.py
@@ -0,0 +1,12 @@
+'''
+Author: Yifan Tang
+Date: 2021-09-05 13:19:13
+LastEditTime: 2021-09-05 13:19:51
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /smart_home/sensor/human_infrared_sensor.py
+'''
+
+
+if __name__ == "__main__":
+    pass
